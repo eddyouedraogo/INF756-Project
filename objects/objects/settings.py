9110bf61e60a0b57a93bd6b53c1d540052b3b7f8
msattoure <msattoure@gmail.com>
2023-10-01 11:43:21 -0400

    docs: swagger integration

--- objects/objects/settings.py
+++ objects/objects/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_swagger',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
